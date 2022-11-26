e4e0de51435f4fc9a6d945873660e1b10e64f703
tuhin902 <tuhin902md@gmail.com>
2022-11-26 20:11:34 +0600

    login and signup route added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Toaster } from 'react-hot-toast';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes/Routes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="max-w-[1440px] mx-auto" >
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,78 @@
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+
+const SignUp = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { createUser } = useContext(AuthContext);
+    const [signUpError, setSignUpError] = useState('');
+    // const [createdUserEmail, setCreatedUserEmail] = useState('');
+    // const [token] = useToken(createdUserEmail);
+    const navigate = useNavigate();
+
+    // if (token) {
+    //     navigate('/');
+    // }
+
+
+    const handleSignUp = (data) => {
+        console.log(data)
+        setSignUpError('');
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                navigate('/');
+                toast('user Created Succesfully.')
+            })
+            .catch(error => {
+                console.error(error)
+                setSignUpError(error.message)
+            });
+
+    }
+
+    return (
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className="text-xl text-center">Signup</h2>
+                <form onSubmit={handleSubmit(handleSignUp)} >
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Name</span></label>
+                        <input type="text" {...register("name", {
+                            required: "Name is requires"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="text" {...register("email", {
+                            required: "Email is required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" {...register("password", {
+                            required: "Password is required",
+                            minLength: { value: 6, message: "password must be 6 character long" },
+                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must be strong' }
+                        })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                    </div>
+                    <input className='btn btn-accent w-full mt-4' value='Signup' type="submit" />
+                    {signUpError && <p className='text-red-600'>{signUpError}</p>}
+                </form>
+                <p>Already have an account <Link className='text-secondary' to='/login'>Please Login</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
