959f2990ee6441ff70302a92a416b70f9963eacb
tuhin902 <tuhin902md@gmail.com>
2022-12-01 18:03:14 +0600

    booked products data added

--- src/Pages/Dashboard/BookedProducts/BookedProducts.js
+++ src/Pages/Dashboard/BookedProducts/BookedProducts.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
 
 const BookedProducts = () => {
+
+    const { user } = useContext(AuthContext);
+
+    const url = `http://localhost:4000/bookings?email=${user?.email}`;
+
+    const { data: bookings = [] } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url)
+            const data = await res.json();
+            return data;
+        }
+    })
+
     return (
         <div>
             <h3 className="text-3xl font-bold m-5">My Booked Products</h3>
@@ -11,32 +27,26 @@ const BookedProducts = () => {
                         <tr>
                             <th></th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Item</th>
+                            <th>Price </th>
+                            <th>Phone </th>
+                            <th>Address </th>
+                            <th>Meeting Location </th>
                         </tr>
                     </thead>
                     <tbody>
-
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-
-                        <tr className="hover">
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
+                        {
+                            bookings.map((booking, i) =>
+                                <tr key={booking._id}>
+                                    <th>{i + 1}</th>
+                                    <td>{booking.name}</td>
+                                    <td>{booking.item}</td>
+                                    <td>${booking.price}</td>
+                                    <td>{booking.phone}</td>
+                                    <td>{booking.address}</td>
+                                    <td>{booking.location}</td>
+                                </tr>)
+                        }
                     </tbody>
                 </table>
             </div>
